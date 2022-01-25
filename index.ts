230babbd264a0a02b46084f038ed02721e529d89
honeykikiki <tjdwls75666@naver.com>
2022-01-25 17:14:49 +0900

    회사 메인 홈페이지레이아웃  디자인 완료

---
+++ index.ts
@@ -0,0 +1,11 @@
+const $mainSectionTwoViewmore = document.querySelector(".main_section2_viewmore") as HTMLDivElement;
+
+$mainSectionTwoViewmore.addEventListener("mouseenter", () => {
+  if ($mainSectionTwoViewmore.classList[1] === "slide-in-left") {
+    $mainSectionTwoViewmore.classList.remove("slide-in-left");
+  } else {
+    $mainSectionTwoViewmore.classList.add("slide-in-left");
+  }
+});
+
+console.log(window.screen);
